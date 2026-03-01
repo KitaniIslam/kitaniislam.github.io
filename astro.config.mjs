cfdb04bcfc879f1f4195dc65b9258370c3f5ec70
Kitani Islam <slmkitani@gmail.com>
2026-03-02 04:23:51 +0700

    Create seamless footer section

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
 export default defineConfig({
   output: 'static',
   site: 'https://kitani.github.io',
-  base: isGitHubActions ? '/Portfolio' : '/',
+  base: isGitHubActions ? '/portfolio' : '/',
   integrations: []
 });
